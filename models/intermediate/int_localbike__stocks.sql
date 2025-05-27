bcae9f39d8f9daabfe4e00c80a92f0b6ef6f23e8
EMMANUEL UBEDA <ubeda.civico.manuel@gmail.com>
2025-05-27 12:41:13 +0000

    int_localbike__stocks mrt_localbike__stocks

---
+++ models/intermediate/int_localbike__stocks.sql
@@ -0,0 +1,13 @@
+select 
+  product_name,
+  brand_id,
+  category_id,
+  model_year,
+  list_price,
+  store_name,
+  city,
+  state
+
+from `dbt_localbike.stg_localbike__stocks` AS s
+INNER JOIN `dbt_localbike.stg_localbike__products` AS p ON s.product_id=p.product_id
+INNER JOIN `dbt_localbike.stg_localbike__stores` As st ON s.store_id=st.store_id 
